ed425b0721947a50e17cfc36d1c306cdd0035157
zbruce13 <zbruce13@gmail.com>
2025-09-03 10:03:59 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,2 @@
-// scripts.js
-
-// Set copyright year
-const yearSpan = document.getElementById("year");
-const currentYear = new Date().getFullYear();
-yearSpan.textContent = currentYear;
-
-// Set last modified date
-const lastModifiedSpan = document.getElementById("lastModified");
-const lastModifiedDate = new Date(document.lastModified);
-lastModifiedSpan.textContent = lastModifiedDate.toLocaleString();
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
