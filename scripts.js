36cb83905bb5d0c8a8a9a5b1ddfe39192a80ec28
zbruce13 <zbruce13@gmail.com>
2025-09-03 10:19:01 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,6 @@
-document.getElementById("year").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = document.lastModified;
+document.addEventListener("DOMContentLoaded", () => {
+  const lastModified = document.getElementById("lastModified");
+  if (lastModified) {
+    lastModified.textContent = `Last Modified: ${document.lastModified}`;
+  }
+});
